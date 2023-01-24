438bf2905f4963fc844e72b15d5ac28326dbee9f
TaherSalah <tahersalah2016@gmail.com>
2023-01-25 00:14:51 +0200

    note cubit methods

--- lib/shared/cubit/cubit.dart
+++ lib/shared/cubit/cubit.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:notes/models/archived/archived_screen.dart';
 import 'package:notes/models/done/done_screen.dart';
 import 'package:notes/models/home_notes/notes_screen.dart';
 import 'package:notes/models/settings/settings_screen.dart';
+import 'package:notes/moduals/notes_models/notes_model_data.dart';
 import 'package:notes/shared/components/widgets/add_bottom_sheet.dart';
 import 'package:notes/shared/cubit/state.dart';
+import 'package:notes/shared/style/constance.dart';
 
 
 class NotesCubit extends Cubit<NotesStates> {
@@ -13,11 +16,40 @@ class NotesCubit extends Cubit<NotesStates> {
 
 static NotesCubit get(context) =>BlocProvider.of(context);
 
+
+
+
+ addNotes(NotesModel notesModel){
+   emit(NotesLoadingState());
+   var notesBox=Hive.box<NotesModel>(kNotesBox);
+   notesBox.add(notesModel).then((value) {
+     emit(NotesSuccessState());
+     // ignore: avoid_print
+     print(value.toString());
+   }).catchError((error){
+     emit(NotesErrorState(error));
+     // ignore: avoid_print
+     print(error.toString());
+   });
+   
+ }
+
+
+
+
+
+
+
+
+
+
+
+
   int currentIndex = 0;
   List<Widget> screens = [
     const LayoutHome(),
     const ArchivedScreen(),
-    // const AddNoteBottomSheet(),
+    const AddNoteBottomSheet(),
     const DoneScreen(),
     const SettingsScreen(),
   ];
